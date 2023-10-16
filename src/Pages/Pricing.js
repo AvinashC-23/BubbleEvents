b8d0e680d942e4e647776c074e6fc21b89db27f9
Avinash C <avinashc212002@gmail.com>
2023-10-16 23:10:42 +0530

    Form Check

--- src/Pages/Pricing.js
+++ src/Pages/Pricing.js
@@ -1,10 +1,45 @@
 import React from 'react'
 import "./pricing.css"
 function Pricing() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    try {
+      await fetch("/", {
+        method: "POST",
+        body: formData,
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      });
+
+      alert("Form submitted successfully!");
+    } catch (error) {
+      alert("Error submitting form!");
+    }
+  };
+
   return (
     <section className='pricingSection' id="pricing">
         <h1 className='blackHeading'>Get the best prices in Bangalore</h1>
         <button className='button-36'><a href='https://api.whatsapp.com/send?phone=919916050428&text=Hello!'>GET QUOTE</a></button>
+        <form name="contact" method="post" onSubmit={handleSubmit}>
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label htmlFor="name">Name</label> <br />
+        <input type="text" id="name" name="name" required />
+      </p>
+      <p>
+        <label htmlFor="email">Email</label> <br />
+        <input type="email" id="email" name="email" required />
+      </p>
+      <p>
+        <label htmlFor="message">Message</label> <br />
+        <textarea id="message" name="message" required></textarea>
+      </p>
+      <p>
+        <button type="submit">Submit message</button>
+      </p>
+    </form>
     </section>
   )
 }
